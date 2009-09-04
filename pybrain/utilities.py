4a888570bfb9479344a42b5ed691322bb615c5c0
Justin S Bayer <bayer.justin@googlemail.com>
2009-09-04 11:50:31 +0200

    Added GvR's multimethod decorator.

--- pybrain/utilities.py
+++ pybrain/utilities.py
@@ -6,6 +6,7 @@ import gc
 import pickle
 import logging
 import threading
+import types
 import os
 import operator
 
@@ -590,3 +591,38 @@ def avgFoundAfter(decreasingTargetValues, listsOfActualValues, batchSize = 1):
 
 class DivergenceError(Exception):
     """ Raised when an algorithm diverges. """
+
+
+
+# This code is taken from Guido van Rossum's weblog.
+# http://www.artima.com/weblogs/viewpost.jsp?thread=101605
+
+_multimethod_registry = {}
+
+class MultiMethod(object):
+    def __init__(self, name):
+        self.name = name
+        self.typemap = {}
+    def __call__(self, *args):
+        types = tuple(arg.__class__ for arg in args)
+        function = self.typemap.get(types)
+        if function is None:
+            raise TypeError("no match for %s" % str(types))
+        return function(*args)
+    def register(self, types, function):
+        if types in self.typemap:
+            raise TypeError("duplicate registration")
+        self.typemap[types] = function
+
+
+def multimethod(*types):
+    def register(function):
+        function = getattr(function, "__lastreg__", function)
+        name = function.__name__
+        mm = _multimethod_registry.get(name)
+        if mm is None:
+            mm = _multimethod_registry[name] = MultiMethod(name)
+        mm.register(types, function)
+        mm.__lastreg__ = function
+        return mm
+    return register
