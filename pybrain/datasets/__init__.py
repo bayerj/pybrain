beebdc632e36acab0d6119c3f5dbc8b61735e67c
Justin S Bayer <bayer.justin@googlemail.com>
2009-12-01 20:48:27 +0100

    Fixed some imports.

--- pybrain/datasets/__init__.py
+++ pybrain/datasets/__init__.py
@@ -1,4 +1,6 @@
 from datasets import SequenceRegressionDataSet, RegressionDataSet
+from containers import Vectors, Scalars, Sequences
+from base import DataSet
 
 # To prevent import errors during development.
 SequentialDataSet = None
