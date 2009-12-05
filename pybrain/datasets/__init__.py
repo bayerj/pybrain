b84ed42b79a0acfc7732f3da6bdd19647b4a5d18
Justin S Bayer <bayer.justin@googlemail.com>
2009-12-05 13:50:56 +0100

    Made internal package names available.

--- pybrain/datasets/__init__.py
+++ pybrain/datasets/__init__.py
@@ -1,6 +1,9 @@
-from datasets import SequenceRegressionDataSet, RegressionDataSet
+from datasets import (ImportanceSequenceRegressionDataSet, 
+                      SequenceRegressionDataSet, 
+                      RegressionDataSet)
 from containers import Vectors, Scalars, Sequences
 from base import DataSet
+from helpers import replaceNansByMeans
 
 # To prevent import errors during development.
 SequentialDataSet = None
